bcdef0b4865bec9a62a020a2df355bc548104ac1
wailsb <wail.saribey@gmail.com>
2026-04-01 17:13:43 +0100

    added env get var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
     setIsTyping(true);
 
     try {
-      // Connect to the backend on Port 5070 (unrestricted in Firefox)
-      const response = await fetch("http://localhost:5070/assistant/chat", {
+      // Connect to the backend from env vars
+      const response = await fetch(process.env.BACKEND_URL as string, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
